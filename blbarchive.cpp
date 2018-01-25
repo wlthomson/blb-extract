f619bb3972346f40452b54c8575d48b20e490e26
Will Thomson <will.tho@outlook.com>
2018-01-26 09:10:40 +1300

    Process file entries

--- blbarchive.cpp
+++ blbarchive.cpp
@@ -17,6 +17,46 @@ namespace Blb {
     _fs.read((char*)&_header.fileCount, sizeof(uint32_t));
 
     // TODO: check archive validity
+
+    _fileIds.reserve(_header.fileCount);
+    for (uint32_t i = 0; i < _header.fileCount; i++) {
+      uint32_t fileId;
+      _fs.read((char*)&fileId, sizeof(uint32_t));
+      _fileIds.push_back(fileId);
+    }
+
+    uint16_t* shiftOffsets = (uint16_t*)malloc(sizeof(uint16_t) * _header.fileCount);
+    for (uint32_t i = 0; i < _header.fileCount; i++) {
+      BlbFileEntry entry;
+
+      _fs.read((char*)&entry.type, sizeof(uint8_t));
+      _fs.read((char*)&entry.compr, sizeof(uint8_t));
+      entry.shiftVal = DW_SHIFT_DEFAULT;
+      _fs.read((char*)&shiftOffsets[i], sizeof(uint16_t));
+      _fs.read((char*)&entry.timeStamp, sizeof(uint32_t));
+      _fs.read((char*)&entry.offset, sizeof(uint32_t));
+      _fs.read((char*)&entry.diskSize, sizeof(uint32_t));
+      _fs.read((char*)&entry.outSize, sizeof(uint32_t));
+
+      std::pair<uint32_t, BlbFileEntry> entryPair(_fileIds[i], entry);
+      _fileEntries.insert(entryPair);
+    }
+
+    uint8_t* const shiftArray = (uint8_t*)malloc(_header.shiftSize);
+    _fs.read((char*)shiftArray, _header.shiftSize);
+
+    for (uint32_t i = 0; i < _header.fileCount; i++) {
+      BlbFileEntry &entry = _fileEntries[_fileIds[i]];
+
+      if (shiftOffsets[i] < _header.shiftSize) {
+	entry.shiftVal = shiftArray[shiftOffsets[i]];
+      }
+    }
+
+    free(shiftOffsets);
+    free(shiftArray);
+
+    _fs.seekg(0, std::ios::beg);
   }
 
   BlbArchive::~BlbArchive() {
@@ -32,5 +72,19 @@ namespace Blb {
     std::cerr << "archiveSize: " << std::dec <<  (uint)_header.archiveSize << std::endl;
     std::cerr << "fileCount: " << std::dec << (uint)_header.fileCount << std::endl;
     std::cerr << "---------------" << std::endl;
+
+    for (uint i = 0; i < _header.fileCount; i++) {
+      std::cerr << _filename << " (" << i + 1 << "/" << _header.fileCount << ")" << std::endl;
+      std::cerr << "---------------" << std::endl;
+      std::cerr << "id: " << "0x" << std::hex << (uint)_fileIds[i] << std::endl;
+      std::cerr << "type: " << "0x" << std::hex << (uint)_fileEntries[_fileIds[i]].type << std::endl;
+      std::cerr << "compr: " << "0x" << std::hex << (uint)_fileEntries[_fileIds[i]].compr << std::endl;
+      std::cerr << "shiftVal: " << "0x" << std::hex << (uint)_fileEntries[_fileIds[i]].shiftVal << std::endl;
+      std::cerr << "timeStamp: " << std::dec << (uint)_fileEntries[_fileIds[i]].timeStamp << std::endl;
+      std::cerr << "offset: " << std::dec << (uint)_fileEntries[_fileIds[i]].offset << std::endl;
+      std::cerr << "diskSize: " << std::dec << (uint)_fileEntries[_fileIds[i]].diskSize << std::endl;
+      std::cerr << "outSize: " << std::dec << (uint)_fileEntries[_fileIds[i]].outSize << std::endl;
+      std::cerr << "---------------" << std::endl;
+    }
   }
 }
--- blbarchive.h
+++ blbarchive.h
@@ -2,11 +2,15 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 
 namespace Blb {
 
   static const uint32_t HEADER_ID_1 = 0x02004940;
   static const uint16_t HEADER_ID_2 = 0x0007;
+
+  static const uint8_t DW_SHIFT_DEFAULT = 0xFF;
 
   struct BlbHeader {
     uint32_t id1;
@@ -14,6 +18,16 @@ namespace Blb {
     uint16_t shiftSize;
     uint32_t archiveSize;
     uint32_t fileCount;
+  };
+
+  struct BlbFileEntry {
+    uint8_t type;
+    uint8_t compr;
+    uint8_t shiftVal;
+    uint32_t timeStamp;
+    uint32_t offset;
+    uint32_t diskSize;
+    uint32_t outSize;
   };
 
   class BlbArchive {
@@ -25,5 +39,7 @@ namespace Blb {
     std::string _filename;
     std::ifstream _fs;
     BlbHeader _header;
+    std::vector<uint32_t> _fileIds;
+    std::unordered_map<uint32_t, BlbFileEntry> _fileEntries;
   };
 }
